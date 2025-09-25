492ec4217dbd8309200c9b23cebb28497f174f0d
rupamsutar <rupamsutar281998@gmail.com>
2025-09-25 17:44:19 +0530

    Basic components added

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+
+  return (
+    <>
+      <div className=""><p>Rupam Sutar</p></div>
+    </>
+  )
+}
+
+function Logo() {
+  return <h1>🎄 Far Away 👜</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip? </h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return (
+    <div className="lsit">
+
+    </div>
+  )
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>👜 You have X items on your list, and you already packed X.</em>
+    </footer>
+  )
+}
+
+export default App
